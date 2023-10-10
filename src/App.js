7c1c1c1e1d0443f901cc810d746d35c26252d6d7
debosmita-567 <134045200+debosmita-567@users.noreply.github.com>
2023-10-10 20:33:25 +0530

    table added for fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [searchTodo, setSearchTodo] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const getTodos = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+      setSearchTodo(res.data);
+    });
+  };
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+    if (e.target.value === "") {
+      setTodos(searchTodo);
+    } else {
+      const searchResults = searchTodo.filter((todo) =>
+        todo.title.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setTodos(searchResults);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos Table</h1>
+      <div className="searchDiv">
+        <input
+          type="search"
+          placeholder="Search Todo..."
+          value={searchText}
+          onChange={(e) => {
+            handleSearch(e);
+          }}
+        />
+      </div>
+      {todos.length > 0 ? (
+        <table>
+          <tr id={0}>
+            <th>userId</th>
+            <th>id</th>
+            <th>title</th>
+            <th>completed</th>
+          </tr>
+          {todos.map((todo) => {
+            const { userId, id, title, completed } = todo;
+            return (
+              <>
+                <tr key={id}>
+                  <td>{userId}</td>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                   <td>{completed ? "true" : "false"}</td>
+                </tr>
+              </>
+            );
+          })}
+        </table>
+      ) : (
+        <div>
+          <h2>No Data Found</h2>
+          <h3>Hey Debo</h3>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
